601501586723372cd30f611265c8c11ed16abe1d
SamuelLeiva <leiva20152@gmail.com>
2025-04-16 13:00:47 -0500

    Profile component added and upload banner functionality

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,43 @@
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+import { useState } from "react";
+import { FaCamera } from "react-icons/fa";
 
-export default Profile
+const Profile = () => {
+  const [bannerUrl, setBannerUrl] = useState("https://placehold.co/1500x400");
+
+  const [profileUrl, setProfileUrl] = useState("https://placehold.co/100");
+
+  const handleBannerChange = (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      setBannerUrl(URL.createObjectURL(file));
+    }
+  };
+
+  return (
+    <div className="relative w-[94%] ml-[5rem]">
+      <div className="relative">
+        <img
+          src={bannerUrl}
+          alt="background image"
+          className="w-full h-60 object-cover"
+        />
+
+        <button className="absolute top-2 right-2 bg-gray-800 text-white p-2 rounded-full hover:bg-gray-600">
+          <label htmlFor="banner-upload" className="cursor-pointer">
+            <FaCamera size={24} />
+          </label>
+          <input
+            type="file"
+            id="banner-upload"
+            accept="image/*"
+            className="hidden"
+            onChange={handleBannerChange}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
