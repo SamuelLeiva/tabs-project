5c53c0bff9bf5bae7ac62abf4b7abec89b19e702
SamuelLeiva <leiva20152@gmail.com>
2025-04-16 13:38:39 -0500

    Added tabs component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaCamera } from "react-icons/fa";
+import Tabs from "./Tabs";
 
 const Profile = () => {
   const [bannerUrl, setBannerUrl] = useState("https://placehold.co/1500x400");
@@ -15,11 +16,11 @@ const Profile = () => {
   };
 
   const handleProfileChange = (event: any) => {
-    const file = event.target.files[0]
-    if(file) {
-        setProfileUrl(URL.createObjectURL(file))
+    const file = event.target.files[0];
+    if (file) {
+      setProfileUrl(URL.createObjectURL(file));
     }
-  }
+  };
 
   return (
     <div className="relative w-[94%] ml-[5rem]">
@@ -44,6 +45,8 @@ const Profile = () => {
         </button>
       </div>
 
+      {/* Profile photo handling */}
+
       <div className="flex items-center ml-4 mt-[2rem]">
         <img
           src={profileUrl}
@@ -51,12 +54,32 @@ const Profile = () => {
           className="w-40 h-40 object-cover rounded-full border-white relative"
         />
         <button className="-ml-[6rem] mt-[9rem] z-10 bg-gray-800 text-white p-2 rounded-full hover:bg-gray-600">
-            <label htmlFor="profile-upload" className="cursor-pointer">
-                <FaCamera size={24} />
-            </label>
-            <input type="file" id="profile-upload" accept="image/*" className="hidden" onChange={handleProfileChange} />
+          <label htmlFor="profile-upload" className="cursor-pointer">
+            <FaCamera size={24} />
+          </label>
+          <input
+            type="file"
+            id="profile-upload"
+            accept="image/*"
+            className="hidden"
+            onChange={handleProfileChange}
+          />
         </button>
+
+        {/* Channel Details */}
+        <div className="ml-4 mt-4">
+          <h1 className="text-2xl font-bold ml-[4rem]">Sam WebDev</h1>
+          <p className="ml-[4rem]">1M views</p>
+          <p className="mt-2 ml-[4rem]">
+            This is a short description of my channel.
+          </p>
+          <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500">
+            Subscribe
+          </button>
+        </div>
       </div>
+
+      <Tabs />
     </div>
   );
 };
